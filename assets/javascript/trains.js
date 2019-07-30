a7521834532026ac279e2e1fd9f98333d8adea33
Gerard Pelly <gerardwp@gmail.com>
2019-07-31 01:41:38 +1000

    got the function working to update the time every minute

--- assets/javascript/trains.js
+++ assets/javascript/trains.js
@@ -13,7 +13,7 @@ firebase.initializeApp(firebaseConfig);
 var database = firebase.database();
 
 function updateClock() {
-    var theTime = moment().format('LT');
+    var theTime = moment().format('LTS');
     $('#clock').html(theTime);
 };
 
@@ -22,71 +22,76 @@ setInterval(function () {
     updateClock();
 }, 1000);
 
+var storedTime = [];
 
+function updateTimes() {
+    var y = 1;
+    if (storedTime.length > 0) {
+        for (var i = 0; i < storedTime.length; ++i) {
+            var firstTime = storedTime[i][0];
+            var freq = storedTime[i][1];
+            var convTime = moment(firstTime, "HH:mm");
+            var timeDiff = moment().diff(moment(convTime), "minutes");
+            var remainder = timeDiff % freq;
+            var minsTillArrival = freq - remainder;
+            var nextArrivalMins = moment().add(minsTillArrival, "minutes");
+            var nextTrainClock = moment(nextArrivalMins).format("hh:mm");
+            $("#mins-" + y).html(minsTillArrival);
+            $("#next-" + y).html(nextTrainClock);
+            y++
+        }
+    } else return;
+};
 
-// var currentTime = moment().format("HH:mm");
-
-// console.log(currentTime)
-
+setInterval(function () {
+    updateTimes();
+}, 1000);
 
 $("#submit").on("click", function (event) {
-
     event.preventDefault();
-
     var trainName = $("#trainName").val().trim();
     var destination = $("#destination").val().trim();
     var firstTrainTime = $("#firstTrain").val().trim();
     var frequency = $("#frequency").val().trim();
-
     var trainObject = {
         trainName: trainName,
         destination: destination,
         firstTrainTime: firstTrainTime,
         frequency: frequency
     };
-
     database.ref("/trains").push(trainObject);
-
     $("#trainName").val("");
     $("#destination").val("");
     $("#firstTrain").val("");
     $("#frequency").val("");
-
 });
 
+var count = 1;
 database.ref("/trains").on("child_added", function (snap) {
-
-
     var sv = snap.val();
     var key = snap.ref.key;
-
     var convTime = moment(sv.firstTrainTime, "HH:mm");
-
     var timeDiff = moment().diff(moment(convTime), "minutes");
-
     var remainder = timeDiff % sv.frequency;
-
     var minsTillArrival = sv.frequency - remainder;
-
     var nextArrivalTime = moment().add(minsTillArrival, "minutes");
     var nextTrainTime = moment(nextArrivalTime).format("hh:mm");
-
     var row = $('<tr id="' + key + '">');
     var tdName = $('<td class="table-info">' + sv.trainName + '</td>');
     var tdDest = $('<td class="table-info">' + sv.destination + '</td>');
-    var tdFreq = $('<td style="width: 18%">' + sv.frequency + '<span> min</span>' + '</td>');
-    var tdArrTime = $('<td style="width: 17%">' + nextTrainTime + '</td>');
-    var tdArrMin = $('<td style="width: 17%">' + minsTillArrival + '</td>');
+    var tdFreq = $('<td style="width: 18%;" id="freq-' + count + '">' + sv.frequency + '<span> min</span>' + '</td>');
+    var tdArrTime = $('<td style="width: 17%" id="next-' + count + '">' + nextTrainTime + '</td>');
+    var tdArrMin = $('<td style="width: 17%" id="mins-' + count + '">' + minsTillArrival + '</td>');
     var tdRemove = $('<td style="width: 18%">');
     var rmvButton = $("<button>");
     rmvButton.attr("data", key).attr('id', 'rmvBtn').text("✕");
     tdRemove.append(rmvButton);
     row.append(tdName, tdDest, tdFreq, tdArrTime, tdArrMin, tdRemove);
     $("tbody").append(row);
-
-    console.log(sv);
-    // console.log(key);
-
+    count++
+    var timeFreq = [];
+    timeFreq.push(sv.firstTrainTime, sv.frequency)
+    storedTime.push(timeFreq);
 }, function (errorObject) {
     console.log("Error handled: " + errorObject.code);
 });
